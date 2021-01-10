67524a946bf3b223c3904a2541a3b42a57e35b22
Priyansh Jain <48293372+ErR0rpj@users.noreply.github.com>
2021-01-10 13:17:15 +0530

    UI started

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var searchController = TextEditingController();
+  int _currentIndex = 0;
+  Widget appBarTitle = Text(
+    'Name Socho',
+  );
+  Icon searchOrCrossIcon = Icon(Icons.search);
+  Icon filterIcon = Icon(
+    Icons.filter_list_alt,
+    color: Colors.indigo,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    searchController.clear();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: appBarTitle,
+        centerTitle: true,
+        actions: <Widget>[
+          Row(
+            children: [
+              _currentIndex == 0
+                  ? IconButton(
+                      icon: searchOrCrossIcon,
+                      onPressed: () {
+                        setState(() {
+                          if (this.searchOrCrossIcon.icon == Icons.search) {
+                            this.searchOrCrossIcon = Icon(Icons.close);
+                            this.appBarTitle = TextField(
+                              controller: searchController,
+                              //use focus node if autofoucs is not working.
+                              autofocus: true,
+                              onChanged: (value) {},
+                              style: TextStyle(color: Colors.black),
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.search,
+                                  color: Colors.black,
+                                ),
+                                hintText: 'Search...',
+                              ),
+                            );
+                          } else {
+                            searchOrCrossIcon = Icon(Icons.search);
+                            this.appBarTitle = Text('Jain Songs');
+                            searchController.clear();
+                          }
+                        });
+                      })
+                  : Icon(null),
+              _currentIndex == 0
+                  ? IconButton(
+                      icon: filterIcon,
+                      onPressed: () {},
+                    )
+                  : Icon(null),
+            ],
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.chartLine),
+            label: 'Trending',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.edit),
+            label: 'Edit',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.archive),
+            label: 'Playlists',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.wrench),
+            label: 'Settings',
+          ),
+        ],
+        selectedItemColor: Color(0xFF54BEE6),
+        unselectedItemColor: Color(0xFF212323),
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+            if (index == 1) {
+              appBarTitle = Text('');
+            } else if (index == 2) {
+              appBarTitle = Text('Playlists');
+            } else if (index == 3) {
+              appBarTitle = Text('Settings and More');
+            } else {
+              appBarTitle = Text('NAme Socho');
+              this.searchOrCrossIcon = Icon(Icons.search);
+            }
+          });
+        },
+      ),
+      //Disabling IndexedStack- use to store state of its children here used for bottom navigation's children.
+      body: <Widget>[][_currentIndex],
+    );
+  }
+}
